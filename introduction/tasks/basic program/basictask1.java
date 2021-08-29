6617272e31257810fd2e33c76ad17723de6dd57a
Savka Oleg <oveguk@gmail.com>
2021-08-29 16:31:21 +0300

    Added 2 tasks

---
+++ introduction/tasks/basic program/basictask1.java
@@ -0,0 +1,7 @@
+class basictask1 {
+  public static int doubleInteger(int i){
+    i =4;
+    // Double the integer and return it!
+    return i;
+  }
+}
---
+++ introduction/tasks/basic program/basictask2.java
@@ -0,0 +1,9 @@
+public class basictask2 {
+   
+    public static void main(String[] args) {
+           
+        int x=5;
+        int y=6;
+        System.out.println(x*y);
+    }
+}
